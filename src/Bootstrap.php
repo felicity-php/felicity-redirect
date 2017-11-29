8547bcff7b18d028cf33bce990dcc8ac2cd62081
TJ Draper <tj@buzzingpixel.com>
2017-11-28 19:53:21 -0600

    Add twig bootstrapping functionality

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,8 +1,31 @@
 <?php
 
 /**
- * Felicity Redirect bootstrap
  * @author TJ Draper <tj@buzzingpixel.com>
  * @copyright 2017 BuzzingPixel, LLC
  * @license Apache-2.0
  */
+
+namespace felicity\redirect;
+
+/**
+ * Class Bootstrap
+ */
+class Bootstrap
+{
+    public function run()
+    {
+        if (! class_exists('\felicity\twig\Twig')) {
+            return;
+        }
+
+        \felicity\twig\Twig::get()->addFunction(
+            new \Twig_function(
+                'redirect',
+                function (string $location, int $code = 301) {
+                    Redirect::to($location, $code);
+                }
+            )
+        );
+    }
+}
